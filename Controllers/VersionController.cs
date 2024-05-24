e4f42f49d0945861537320b474bd5ac03410df6c
ozer.erol <ozer.erol@boyner.com.tr>
2024-05-25 00:24:58 +0300

    Add project files.

---
+++ Controllers/VersionController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JenkinsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VersionController : ControllerBase
+    {
+        public IConfiguration Configuration { get; set; }
+        public VersionController(IConfiguration configuration) { 
+        
+            Configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(($"Api Version : {Configuration.GetSection("ApiVersion").Value}"));
+        }
+    }
+}
