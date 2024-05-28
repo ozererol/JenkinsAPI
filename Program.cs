1cbe6ceb58866224a26a9da18833415c0059b483
ozer.erol <ozer.erol@boyner.com.tr>
2024-05-29 00:26:46 +0300

    health check eklendi

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHealthChecks();
 
 var app = builder.Build();
 
@@ -17,6 +18,8 @@ var app = builder.Build();
 //}
 
 //app.UseHttpsRedirection();
+
+app.UseHealthChecks("/health");
 
 app.UseAuthorization();
 
